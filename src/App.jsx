6fd3694afd9bac1161a38fe2744885c408b57394
unknown <toan9370@donga.edu.vn>
2025-08-28 15:04:14 +0700

    first commit create project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="border-4 border-black h-screen">
+        <div className="border-4 border-black h-[90%]">hello</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
