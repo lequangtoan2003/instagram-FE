01e42d7013622f7a6d287da988cbaaeed87b4d38
unknown <toan9370@donga.edu.vn>
2025-08-28 22:35:06 +0700

    signup-login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import "./App.css";
-import { Button } from "./components/ui/button";
-
+import Home from "./components/Home";
+import Login from "./components/Login";
+import MainLayout from "./components/MainLayout";
+import Profile from "./components/Profile";
+import Signup from "./components/Signup";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const browserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/profile", element: <Profile /> },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
 function App() {
   return (
     <>
-      <div className="border-4 border-black h-screen">
-        <div className="border-4 border-black h-[90%]">hello</div>
-      </div>
+      <RouterProvider router={browserRouter} />
     </>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import axios from "axios";
+import { toast } from "sonner";
+import { Link, useNavigate } from "react-router-dom";
+import { Loader2 } from "lucide-react";
+
+export default function Login() {
+  const navigate = useNavigate();
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const changeEvantHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+  const signupHandler = async (e) => {
+    e.preventDefault();
+    console.log(input);
+    try {
+      setLoading(true);
+      const res = await axios.post(
+        "http://localhost:8001/api/v1/user/login",
+        input,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      if (res.data.success) {
+        navigate("/");
+        toast.success(res.data.message);
+        setInput({ email: "", password: "" });
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <form
+        onSubmit={signupHandler}
+        className="shadow-lg flex flex-col gap-4 p-8"
+      >
+        <div className="my-2">
+          <h1 className="text-center font-bold text-xl">LOGO</h1>
+          <p className="text-sm text-center">
+            Login to see photo & videos from your friends
+          </p>
+        </div>
+        <div className="">
+          <span className="font-medium">Email</span>
+          <Input
+            type="email"
+            name="email"
+            value={input.email}
+            onChange={changeEvantHandler}
+            className="focus-visible:ring-transparent my-2"
+          />
+        </div>
+        <div className="">
+          <span className="font-medium">Password</span>
+          <Input
+            type="password"
+            name="password"
+            value={input.password}
+            onChange={changeEvantHandler}
+            className="focus-visible:ring-transparent my-2"
+          />
+        </div>
+        {loading ? (
+          <Button>
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+          </Button>
+        ) : (
+          <Button type="submit" className="my-2">
+            Login
+          </Button>
+        )}
+        <span className="text-center">
+          Dosent have an account?
+          <Link to="/signup" className="text-blue-600">
+            Signup
+          </Link>
+        </span>
+      </form>
+    </div>
+  );
+}
