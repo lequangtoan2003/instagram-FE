b8661715e7df754a08343b0b6fb18ebcff6662f0
unknown <toan9370@donga.edu.vn>
2025-08-29 00:53:14 +0700

    sidebar-and-logout

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import LeftSidebar from "./LeftSidebar";
 
 export default function MainLayout() {
   return (
     <div>
-      Sidebar
+      <LeftSidebar />
       <Outlet />
     </div>
   );
